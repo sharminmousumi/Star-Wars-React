a76cd30905c66cc963ea5886b9c7c4e5093d62ed
Sharmin <Mousumi@MdAlamgirs-MacBook-Pro.local>
2020-06-06 19:37:44 +0200

    Updates

---
+++ precache-manifest.4ea3e2958a7aae0ce7c049711565666f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678e0f85de172546298e5d3b2a0705d8",
+    "url": "/star-wars/index.html"
+  },
+  {
+    "revision": "2a021b55f482d63ac520",
+    "url": "/star-wars/static/css/main.fab8c8b4.chunk.css"
+  },
+  {
+    "revision": "3eb0e94e5eaf8f242b05",
+    "url": "/star-wars/static/js/2.3b02f017.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/star-wars/static/js/2.3b02f017.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a021b55f482d63ac520",
+    "url": "/star-wars/static/js/main.b6b8fa30.chunk.js"
+  },
+  {
+    "revision": "096feba1be5e75c75265",
+    "url": "/star-wars/static/js/runtime-main.f2265df0.js"
+  }
+]);
