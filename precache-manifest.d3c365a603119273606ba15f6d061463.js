e365a29710425b6d63d4a288e7d80a9f0e167e4d
Sharmin <Mousumi@MdAlamgirs-MacBook-Pro.local>
2020-06-07 09:48:34 +0200

    Updates

--- precache-manifest.d3c365a603119273606ba15f6d061463.js
+++ precache-manifest.d3c365a603119273606ba15f6d061463.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c5c34abb7339e2027228c5be8739f378",
+    "revision": "8d79d295a30d8d72f9d954c52d48e481",
     "url": "/star-wars/index.html"
   },
   {
-    "revision": "dbce55964bb317361758",
-    "url": "/star-wars/static/css/main.219b2b63.chunk.css"
+    "revision": "e6d7e4025c2154c52dac",
+    "url": "/star-wars/static/css/main.28ae6de4.chunk.css"
   },
   {
     "revision": "3eb0e94e5eaf8f242b05",
@@ -16,11 +16,15 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/star-wars/static/js/2.3b02f017.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "dbce55964bb317361758",
+    "revision": "e6d7e4025c2154c52dac",
     "url": "/star-wars/static/js/main.b6b8fa30.chunk.js"
   },
   {
     "revision": "096feba1be5e75c75265",
     "url": "/star-wars/static/js/runtime-main.f2265df0.js"
+  },
+  {
+    "revision": "140e6fd8f69f2df33cd1d69506016ec1",
+    "url": "/star-wars/static/media/star-wars.140e6fd8.jpg"
   }
 ]);
